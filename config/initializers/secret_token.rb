4bc370b02e86674f5d030faa60d34ded08901e3d
Ryan Mawby <ryan@bigbeam.co.uk>
2011-03-07 16:28:09 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unshrink::Application.config.secret_token = '8ddfa35788b052e502f99fd323da9b3556deaa5a528a1b4e3598a696a5e09172b7dbd20bb416c907111808c6647b36dca706e6802dcd04b3475675275b272361'
